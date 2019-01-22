fe734f5b1ce316a4e246e22e0c2bf50137f859fd
wuluchaoren <38044760+wuluchaoren@users.noreply.github.com>
2019-01-22 22:26:08 +0800

    【修改配置文件】

--- src/main/java/com/su/blog/BlogApplication.java
+++ src/main/java/com/su/blog/BlogApplication.java
@@ -1,9 +1,15 @@
 package com.su.blog;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.su.blog.mapper")
+@EnableTransactionManagement
+@EnableAutoConfiguration
 public class BlogApplication {
 
     public static void main(String[] args) {
